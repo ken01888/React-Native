e86d0c25121f449f7ac4c710f3b0828bf79a4aeb
Kenneth  Cunningham <kennethcunningham@Kenneths-iMac.local>
2021-04-25 16:45:14 -0400

    App

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+interface IApp { }
+export default function App<IApp>(props: IApp) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <View style={styles.wrap}>
+      <View style={styles.container}>
+        <Text style={styles.text}>#crushit</Text>
+        <StatusBar style="auto" />
+      </View>
+      </View>
     </View>
+
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  text: {
+    color: 'white',
+    backgroundColor: 'red',
+    fontSize: 50
+  },
+  main:{
+    flex:1,
+    backgroundColor:'red',
+    padding:20,
+    marginTop:50
+  },
+  wrap:{
+    flex:1,
+    backgroundColor:'black',
+    padding:10
+  }
 });
+
+
